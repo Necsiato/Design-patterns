37eb1b85fdcc1fa7a498af87746c2682a4b3b0d8
Denis Klenze <disbalancertram@gmail.com>
2024-09-26 03:27:48 +0500

    Клейн Денис. АИС-22-1. Задание 3

--- state.py
+++ state.py
@@ -41,7 +41,10 @@ class HasQuarterState(State):
 
     def turn_crank(self):
         print("You turned...")
-        self.gumball_machine.set_state(self.gumball_machine.sold_state)
+        if self.gumball_machine.count > 1 and self.gumball_machine.is_winner():
+            self.gumball_machine.set_state(self.gumball_machine.winner_state)
+        else:
+            self.gumball_machine.set_state(self.gumball_machine.sold_state)
 
     def dispense(self):
         print("No gumball dispensed.")
@@ -67,6 +70,32 @@ class SoldState(State):
             print("Oops, out of gumballs!")
             self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
 
+class WinnerState(State):
+    def __init__(self, gumball_machine):
+        self.gumball_machine = gumball_machine
+
+    def insert_quarter(self):
+        print("Please wait, we're already giving you a gumball.")
+
+    def eject_quarter(self):
+        print("Sorry, you already turned the crank.")
+
+    def turn_crank(self):
+        print("Turning twice doesn't get you another gumball!")
+
+    def dispense(self):
+        print("YOU'RE A WINNER! You get two gumballs for your quarter!")
+        self.gumball_machine.release_ball()
+        if self.gumball_machine.count == 0:
+            self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
+        else:
+            self.gumball_machine.release_ball()
+            if self.gumball_machine.count > 0:
+                self.gumball_machine.set_state(self.gumball_machine.no_quarter_state)
+            else:
+                print("Oops, out of gumballs!")
+                self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
+
 class SoldOutState(State):
     def __init__(self, gumball_machine):
         self.gumball_machine = gumball_machine
@@ -89,6 +118,7 @@ class GumballMachine:
         self.no_quarter_state = NoQuarterState(self)
         self.has_quarter_state = HasQuarterState(self)
         self.sold_state = SoldState(self)
+        self.winner_state = WinnerState(self)
 
         self.count = number_gumballs
         if number_gumballs > 0:
@@ -113,6 +143,14 @@ class GumballMachine:
         if self.count > 0:
             print("A gumball comes rolling out the slot...")
             self.count -= 1
+
+    def is_winner(self):
+        # Здесь рандомное определение победителя, можно заменить на любое другое условие
+        import random
+        return random.randint(0, 9) == 0
+
+    def __str__(self):
+        return f"Gumball Machine [balls: {self.count}, state: {self.state.__class__.__name__}]"
 
 def test_gumball_machine():
     gumball_machine = GumballMachine(5)
